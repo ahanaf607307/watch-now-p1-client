07940b27162189d08fd09f581889b1d3988e10a0
ahanaf607307 <ahanaf607307@gmail.com>
2025-01-26 11:19:53 +0600

    setup done watchnow

---
+++ src/Hala.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Hala() {
+  return (
+    <div>
+    <div className="card bg-base-100 w-96 shadow-sm">
+
+  <div className="card-body">
+    <h2 className="card-title bg-red-500">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Hala
